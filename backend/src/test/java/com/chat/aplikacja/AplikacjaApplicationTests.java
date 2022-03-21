064e7d2aeb9a65218d4c511d993462d4d22d13a1
Mateusz <mateusz.narojek@gmail.com>
2022-03-21 22:38:45 +0100

    commit

---
+++ backend/src/test/java/com/chat/aplikacja/AplikacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.aplikacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
