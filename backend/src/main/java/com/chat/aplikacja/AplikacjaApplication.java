939ebfcd3fbf93726d21ed94a7366f62d990ecf7
Mateusz <mateusz.narojek@gmail.com>
2022-03-27 19:22:58 +0200

    first integration with plugin

--- backend/src/main/java/com/chat/aplikacja/AplikacjaApplication.java
+++ backend/src/main/java/com/chat/aplikacja/AplikacjaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AplikacjaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AplikacjaApplication.class, args);
     }
 
